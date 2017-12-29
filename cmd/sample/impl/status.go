387915c7c0203ca3c01a2a77d2d796a8806a2269
Joan Llopis <jllopisg@gmail.com>
2017-12-29 18:52:55 +0100

    sample: added sample server implementation

---
+++ cmd/sample/impl/status.go
@@ -0,0 +1,39 @@
+package impl
+
+import (
+	"context"
+	"time"
+
+	"bitbucket.org/acbapis/acbapis/lib/go/common"
+	"bitbucket.org/acbapis/acbapis/lib/go/status"
+	"github.com/jllopis/mifo/version"
+)
+
+// StatusService provides a service that offers basic status functionality:
+// - Time -> returns UTC epoch time in nanoseconds precision
+// - Version -> returns the current version of the service
+// - Status -> returns the current status of the service
+type StatusService struct{}
+
+// GetServerTime returns the current UTC server time in nanoseconds
+func (st *StatusService) GetServerTime(ctx context.Context, empty *common.EmptyMessage) (*status.ServerTimeMessage, error) {
+	return &status.ServerTimeMessage{Value: time.Now().UTC().UnixNano()}, nil
+}
+
+// GetVersion returns the current API Version. It is a direct mapping from github.com/coreos/go-semver/semver.Version
+func (st *StatusService) GetVersion(ctx context.Context, empty *common.EmptyMessage) (*common.Version, error) {
+	return &common.Version{
+		Version:    version.Version,
+		APIVersion: version.APIVersion,
+		GitCommit:  version.GitCommit,
+		BuildDate:  version.BuildDate,
+	}, nil
+}
+
+func (st *StatusService) GetGlobalServiceStatus(ctx context.Context, empty *common.EmptyMessage) (*status.ServerStatusMessage, error) {
+	serverStatus := &status.ServerStatusMessage{
+		Status: status.ServerStatus_OK,
+	}
+
+	return serverStatus, nil
+}
---
+++ cmd/sample/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"bitbucket.org/acbapis/acbapis/lib/go/status"
+	"google.golang.org/grpc"
+
+	"github.com/jllopis/mifo"
+	"github.com/jllopis/mifo/cmd/sample/impl"
+	"github.com/jllopis/mifo/log"
+	"github.com/jllopis/mifo/version"
+)
+
+func main() {
+	log.Info("Starting sample server")
+	log.Info("Server version", "value", version.SemVer.String())
+	log.Info("Git tag", "value", version.GitCommit)
+	log.Info("Build", "date", version.BuildDate)
+
+	// var err error
+	// var l net.Listener
+	// if l == nil {
+	// 	// Create the main listener.
+	// 	l, err = net.Listen("tcp", ":3000")
+	// 	if err != nil {
+	// 		log.Err("cant create CMux", "error", err.Error())
+	// 		os.Exit(2)
+	// 	}
+	// }
+
+	// api := mifo.NewMicroServer().WithListener(l)
+	api := mifo.NewMicroServer().SetPort(58000)
+	if api == nil {
+		os.Exit(1)
+	}
+
+	log.Info("Server info", "name", api.Name, "port", api.Port)
+	fmt.Println(api.String())
+
+	// Register middleware interceptors
+	api.UsePrometheus(&mifo.PrometheusInterceptorConfig{EnableTimeHistogram: true})
+	api.UseReflection()
+
+	// Register gRPC services
+	statusRegistrator := func(srv *grpc.Server) {
+		status.RegisterStatusServiceServer(srv, &impl.StatusService{})
+	}
+	api.Register(statusRegistrator)
+
+	log.Info("Serving...")
+	log.Info("Prometheus metrics on port " + api.Port)
+	go api.Serve()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGKILL, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	<-sc
+	api.Stop()
+
+	log.Info("Sample finished")
+}
