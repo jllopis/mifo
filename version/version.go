b6fed2d3e5ef3fb4282bf42b881260ab7a3e2303
Joan Llopis <jllopisg@gmail.com>
2019-05-04 03:44:18 +0200

    simplified version package

--- version/version.go
+++ version/version.go
@@ -1,37 +1,17 @@
-// Copyright 2017 Joan Llopis. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-//
-// version package holds the latest version information following the
-// semver spec (http://semver.org)
 package version
 
-import (
-	"fmt"
-
-	semver "github.com/hashicorp/go-version"
+const (
+	// Name of the service
+	Name = "MifoProject"
+	// APIVersion of the API this service provide
+	APIVersion = "v1"
 )
 
 var (
-	// Name of the service
-	Name = "unknown"
-	// Version of the service
-	Version = "0.0.1"
-	// APIVersion of the API this service provide
-	APIVersion = "unknown"
-	// SemVer is the semantic version object
-	SemVer *semver.Version
+	// Version is the current echo version in SemVer format
+	Version = "v0.0.1"
 	// BuildDate represents the date this service was built
 	BuildDate string
 	// GitCommit is the hash of the git commit
 	GitCommit string
 )
-
-func init() {
-	v, err := semver.NewVersion(Version)
-	if err == nil {
-		SemVer = v
-	} else {
-		fmt.Printf("semver error: %s\n", err)
-	}
-}
